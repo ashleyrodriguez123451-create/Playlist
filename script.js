cccf4a5eb9d3c5458db1615c62c0616ea59b7b29
ashleyrodriguez123451-create <ashleyrodriguez123451@gmail.com>
2026-01-28 12:16:23 -0500

    Add files via upload
    form js

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
-console.log("js console");
-var xhttp = new XMLHttpRequest();
+// console.log("js console");
+// var xhttp = new XMLHttpRequest();
 
+// xhttp.onreadystatechange = function (){
+//     if (xhttp.readyState === 4 && xhttp.status === 200){
+//         let songs = JSON.parse(xhttp.responseText);
+//         console.log(songs[0].title);
+//         console.log(songs[1].artist);
+//         songs.forEach((song) => {
+//             console.log(song);
+// });
+//     }
+// };
+// xhttp.open("GET","music.json",true);
+// xhttp.send();
+
+console.log("js console");
+let data;
+let grid = document.querySelector(".grid");
+
+var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function (){
-    if (xhttp.readyState === 4 && xhttp.status === 200){
-        let songs = JSON.parse(xhttp.responseText);
-        console.log(songs[0].title);
-        console.log(songs[1].artist);
-        songs.forEach((song) => {
-            console.log(song);
-});
+ if (xhttp.readyState === 4 && xhttp.status === 200){
+         data = JSON.parse(xhttp.responseText);
+         console.log(data);
+         data.forEach(function(song){
+            let card = document.createElement("div");
+            card.classList.add("card");
+
+            let textData = 
+            "<div class='song-title'>"+ song.title + "</div>" + 
+            "<span>"+
+            "Artist: " + song.artist + "<br>" +
+            "Genre: " + song.genre + "<br>" +
+            "Image Source: " + song.imgSrc + "<br>" +
+            "</span>";
+
+        card.innerHTML = textData;
+        if (song.imgSrc){
+            card.style.backgroundImage = "url("+ song.imgSrc +")";
+        }
+        grid.appendChild(card);
+
+        });
     }
 };
-xhttp.open("GET","music.json",true);
+ xhttp.open("GET", "music.json", true);
 xhttp.send();
+
+FormData.addEventListener("submit", function(e){
+    e.preventDefault();
+    let title = titleInput.value;
+    let artist = artistInput.value;
+    let genre = genreInput.value;
+    let imgSrc = imgSrcInput.value;
+
+    let newSong = {
+        "id": getNextId(),
+        "title": title,
+        "artist": artist,
+        "genre": genre,
+        "imgSrc": imgSrc
+    };
+    submitData(newSong);
+    FormData.reset();
+});
